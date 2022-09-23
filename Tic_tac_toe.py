eb754fa6c0ccfea087ae135fc88f20fbee916dde
Samuel Casellas <samcomon@gmail.com>
2022-09-23 11:50:09 -0600

    Small refactoring

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -3,10 +3,7 @@ Solo Checkpoint 02
 Author: Samuel Casellas
 '''
 
-
-
-#go through?
-def main():
+def main() -> None:
     ''' Holds the main game loop logic
         Selects a player
         Builds the board
@@ -17,36 +14,24 @@ def main():
     '''
     
     player = "x" # x goes first
-    board = create_board()
+    board = [n for n in range(1,10)]
 
     print("\n\n\nWelcome to Tic Tac Toe!\n")
 
-    while is_winner(board) == None and not is_draw(board):
+    while not (is_winner(board) or is_draw(board)):
         print(display_board(board))
         make_move(player, board)
         player = next_player(player)
         print(display_board(board))
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-    winner_statement = is_winner(board)
-    if winner_statement == None:
-        winner_statement = "A draw!"
+    winner = is_winner(board)
+    if not winner:
+      winner = "A draw!"
 
-    print(f"{winner_statement}\nThanks for playing!\n")
+    print(f"{winner}\nThanks for playing!\n")
 
-
-
-def create_board(): #Complete
-    ''' Creates a list that holds the spots on the board
-        It initializes the positions with the numbers for the person to pick
-        return: the board as a list
-    '''
-    board = []
-    for square in range(9):
-        board.append(square+1)
-    return board
-
-def display_board(board): #Complete
+def display_board(board: list) -> str: 
     ''' Displays the current board
         return: the string to print (I do this so I could test the function if needed)
     '''
@@ -57,7 +42,7 @@ def display_board(board): #Complete
     -+-+-
     7|8|9
     """
-    displayed_board = ""
+    displayed_board = str()
 
     for square in range(9):
         displayed_board += f"{board[square]}"
@@ -69,41 +54,32 @@ def display_board(board): #Complete
             displayed_board += "\n"
             return displayed_board
 
-def is_draw(board): #Complete
+def is_draw(board: list) -> bool: 
     ''' return: True if board is a draw, False if board is still playable '''
-    for square in range(9):
-        if board[square] != "x" and board[square] != "o":
-            return False
-    return True
+    return board.count("o") + board.count("x") == 9
 
-def is_winner(board): #Compelete
+def is_winner(board: list) -> str:
     ''' return: True if someone won, False if there is no winner '''
-
     # of win patterns: 8
-    Win_patterns = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-
+    win_patterns = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     #Check if win for x
-    for pattern in Win_patterns:
-        count = 0
+    for pattern in win_patterns:
+        x_count = 0
+        o_count = 0
         for square in pattern:
             if board[square-1] == 'x':
-                count += 1
-        if count == 3:
+                x_count += 1
+            elif board[square-1] == 'o':
+                o_count += 1
+        if x_count == 3:
             return "Player \"x\" is the winner!"
-
-    #Check if win for o
-    for pattern in Win_patterns:
-        count = 0
-        for square in pattern:
-            if board[square-1] == 'o':
-                count += 1
-        if count == 3:
+        if o_count == 3:
             return "Player \"o\" is the winner!"
 
     # No winner
-    return None
+    return ""
 
-def make_move(player, board): #Complete
+def make_move(player: str, board: list) -> None: 
     ''' Prompts player to select a square to play
         Assigns the player to that board location if it is a legal move
         return: None
@@ -113,18 +89,13 @@ def make_move(player, board): #Complete
             player_move = int(input(f"Player {player}, please select a square:\n\t> "))
             if player_move > 9 or player_move < 1 or board[player_move-1] == "o" or board[player_move-1] == "x":
                 raise TypeError
-            break
+            board[player_move-1] = player
+            return
         except (TypeError, ValueError):
             print("\nInvalid move!\n")
 
-    board[player_move-1] = player
-
-def next_player(current): #Complete
-    ''' return: x if current is o, otherwise x '''
-    if current == "x":
-        return "o"
-    else:
-        return "x"
+def next_player(current_player: str) -> str: 
+    return "o" if current_player == "x" else "x"
 
 # run main if this has been called from the command line
 if __name__ == "__main__":
